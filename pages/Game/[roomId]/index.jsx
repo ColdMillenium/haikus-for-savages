5952bd8c067255a51383d82c2eaccd25ea5e6c43
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-20 16:43:33 -0500

    able to display the user's name in lobby when making a room

--- pages/Game/[roomId]/index.jsx
+++ pages/Game/[roomId]/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import {Text} from '@chakra-ui/react'
+import useStore from '../../../store'
 
 function index() {
+  const username = useStore(store => store.username);
   const router = useRouter();
   const { roomId } = router.query;
-  return <div>Room ID: {roomId}</div>;
+  return <div>
+    <Text fontSize="4xl">{username}'s Room</Text>
+    Room ID: {roomId}
+  </div>;
+
 }
 
 export default index;
