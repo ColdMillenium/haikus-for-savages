08908a140c1ade30c758e35b4893108e3082f170
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-20 15:50:49 -0500

    home page buttons go to /JoinGame & CreateGame

---
+++ pages/JoinGame.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function JoinGame() {
+  return <div>Join game</div>;
+}
+
+export default JoinGame;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Center, Button, Image, Box} from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 
 //https://tenor.com/view/smiling-geicos-caveman-3doors-down-let-me-be-myself-song-smirk-gif-20065301
 export default function Home() {
+  const router = useRouter();
   return (
     <Box className={styles.container}>
       <Head>
@@ -19,8 +21,20 @@ export default function Home() {
         <Center  boxSize='sm'>
           <Image background="white" height={300} width={300} rounded={300}src='https://c.tenor.com/16NyC3oOWT4AAAAi/smiling-geicos-caveman.gif' />
         </Center>
-        <Button width={150} mb={5} mt={50}>Create Game</Button>
-        <Button width={150}>Join Game</Button>
+        <Button 
+          onClick={()=>router.push("/CreateGame")} 
+          width={150} 
+          mb={5} 
+          mt={50}
+        >
+          Create Game
+        </Button>
+        <Button 
+          onClick={()=>router.push("/JoinGame")} 
+          width={150}
+        >
+          Join Game
+        </Button>
       </main>
     </Box>
   )
