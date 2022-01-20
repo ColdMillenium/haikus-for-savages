df6a54d082483bf02b04eaa88241e0c3e6ef7d70
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-20 16:16:16 -0500

    Added input title and button to c reate game page

--- pages/CreateGame.jsx
+++ pages/CreateGame.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import {useState} from 'react';
+import {Input, Text, Center, Stack, Button, Box} from '@chakra-ui/react'
 
 function CreateGame() {
-  return <div>CreateGame</div>;
+  const [name, setName] = useState('')
+  const handleChange = (event) => {
+    let value = event.target.value;
+    if(value.length<=12 && !/\s/.test(value)){
+      setName(value)
+    }
+  }
+  return <Center height="100vh">
+    
+    <Stack spacing={5} p={20} rounded={5} boxShadow='md'>
+      <Text mb={2} fontWeight="bold" fontSize="4xl">New Game</Text>
+      <Box>
+        <Text color="grey">Enter your Username</Text>
+        <Input 
+          
+          width={200}
+          value={name}
+          onChange={handleChange}
+          placeholder='Your username' 
+        />
+      </Box>
+      <Button color="grey" mt={5}>Make Game</Button>
+    </Stack>
+    
+    
+  </Center>;
+
 }
 
 export default CreateGame;
