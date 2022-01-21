97f281b13e28f3c07faaf54f92d566d8d2d84084
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-21 12:03:50 -0500

    made Lobby a seperate component

---
+++ components/Lobby.jsx
@@ -0,0 +1,23 @@
+import {useEffect, useRef} from 'react';
+import { useRouter } from 'next/router';
+import {Text ,Button} from '@chakra-ui/react'
+import useStore from '../store.js'
+
+function Lobby() {
+  
+  const router = useRouter();
+  const username = useStore(store => store.username);
+  const clientId = useStore(store => store.clientId)
+  const sayHi = useStore(store => store.sayHi)
+  const { roomId } = router.query;
+
+  return <div>
+    <Text fontSize="4xl">{username}'s Room</Text>
+    Room ID: {roomId}
+    <Button onClick={sayHi}>Hi</Button>
+    <Text>{clientId}</Text>
+  </div>;
+
+}
+
+export default Lobby;
