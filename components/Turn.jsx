c50de0af593fa1440ee38772fec545c2f8d266b5
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-24 17:59:08 -0500

    only showing pile buttons when speaker. using store cliensRole to determine role now

--- components/Turn.jsx
+++ components/Turn.jsx
@@ -1,36 +1,20 @@
 import React , {useEffect, useState}from 'react';
 import useStore from '../store';
 import Card from './Card'
-import {Center, Stack, Flex, Button} from '@chakra-ui/react'
+import {Center, Stack, Flex, Button, Text} from '@chakra-ui/react'
+import Conditional from './Conditional';
 
 function Turn() {
   const {punisher, speaker, audience, currCard, score} = useStore(store => store.room)
-  const clientId = useStore(store => store.clientId);
+  const {clientId, clientsRole ,room} = useStore(store => store);
 
-
-  const [role, setRole] = useState("")
-  useEffect(() =>{
-    if(punisher.id == clientId){
-      setRole("PUNISHER")
-    }else if(speaker.id == clientId){
-      setRole("SPEAKER")
-    }else{
-      setRole("AUDIENCE");
-    }
-  }, [punisher, speaker])
-  useEffect(() =>{
-    if(currCard){
-
-    }
-  }, [currCard])
-  
   const showCard = () =>{
-    if(role == "SPEAKER"){
+    if(clientsRole == "Speaker"){
       return <Center h="100vh" w="100%">
       <Card hidden={false} easy="Dance" hard ="Belly Dance"/>
       </Center>
       
-    }else if(role == "PUNISHER"){
+    }else if(clientsRole == "Punisher"){
       return <Center h="100vh" w="100%" >
         <Stack></Stack>
       <Card hidden={false} easy="Dance" hard ="Belly dance"/>
@@ -41,17 +25,21 @@ function Turn() {
       </Center>
     }
   }
-  
+  console.log(room);
   return <Center h="100vh" w="100%">
     <Stack h="500px">
       {showCard()}
-      <Flex>
-        <Button m={2} colorScheme="red">Oops -1</Button>
-        <Button m={2} colorScheme="yellow">Easy +1</Button>
-
-        <Button m={2}colorScheme="green">Great +1</Button>
-
-      </Flex>
+      <Conditional condition={clientsRole == "Speaker"}>
+        <Flex>
+          <Button m={2} colorScheme="red">Oops -1</Button>
+          <Button m={2} colorScheme="yellow">Easy +1</Button>
+          <Button m={2}colorScheme="green">Great +1</Button>
+        </Flex>
+      </Conditional>
+      <Conditional condition={clientsRole == "Audience"}>
+        <Text> Listen carefully to the Speaker!</Text>
+      </Conditional>
+      
     </Stack>
     
 
