955650b16e78a3f467ab408e99cb7c12e9d57f97
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-24 00:51:43 -0500

    can see your role and the readiness of other roles on the TurnTransition page

--- components/TurnTransition.jsx
+++ components/TurnTransition.jsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Text ,Button, Box, Flex, Input, Select, Spacer, Center} from '@chakra-ui/react'
+import useStore from '../store'
+
+
 
 function TurnTransition() {
-  return <div>Turn Transition!</div>;
+  const {
+    punisher,
+    speaker,
+    audience,
+    punisherReady,
+    audienceReady,
+    speakerReady,
+    mode,
+  } = useStore(store => store.room)
+  const {username, clientId} = useStore(store => store);
+  const [readyPromptHidden, setReadyPromptHidden] = useState(true);
+  const [role, setRole ] = useState("Audience");
+
+  useEffect(() =>{
+    console.log(clientId, username);
+    console.log(speaker.id);
+    if(username == speaker.username){
+      setRole("Speaker")
+    }else if(punisher && punisher.username == username){
+      setRole("Punisher")
+    }else{
+      setRole("Audience");
+    }
+    console.log(audience,speaker,punisher)
+  },[clientId, audience, speaker, punisher, username])
+  return <>
+    <Center h="100vh" w="100vw">
+      <Flex align="center" justify="center" direction="column">
+        <Text>You are the {role}</Text>
+        <Text>When you're ready to begin, click ready</Text>
+        {mode == "COOP"?<>
+          <Text>[{speakerReady? "Speaker Ready" : "Speaker Not Ready"}]</Text>
+          <Text>[{audienceReady? "Audience Ready" : "Audience Not Ready"}]</Text>
+        </>:
+        mode == "ROTATE"?<>
+          <Text>[{speakerReady? "Speaker Ready" : "Speaker Not Ready"}]</Text>
+          <Text>[{audienceReady? "Audience Ready" : "Audience Not Ready"}]</Text>
+          <Text>[{punisherReady? "Punisher Ready" : "Punisher Not Ready"}]</Text>
+        </>:
+        <>
+          <Text>[{speakerReady? "Speaker Ready" : "Speaker Not Ready"}]</Text>
+          <Text>[{punisherReady? "Punisher Ready" : "Punisher Not Ready"}]</Text>
+        </>
+        }
+        <Text>{audience.ready}</Text>
+        <Button>Ready!</Button>
+      </Flex>
+    </Center>;
+  </>
 }
 
 export default TurnTransition;
