5cefeb9453bac252a3bb1ae639d4aa34d49bbc4c
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-25 15:13:52 -0500

    Card component will let us know if card data is null instead of erroring

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,6 +2,12 @@ import React, {useState} from 'react';
 import {Box, Stack, Text, Flex} from '@chakra-ui/react'
 
 function Card({hidden, card}) {
+  if(!card || !card.GOOD || !card.GREAT){
+    console.log("card:" ,card);
+    card = {}
+    card.GOOD = "null Card?";
+    card.GREAT = "check Logs";
+  }
   return <Box p={10} boxShadow="lg" rounded={5} height="300" minWidth="200">
     <Flex direction="column" align="center" width="100%" h="100%">
       <Text fontSize="2xl" align="center" mb={10}>{hidden? "???": card.GOOD}</Text>
