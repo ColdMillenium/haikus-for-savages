04eca727fbd5b3160cb41818946e70d74a0df1e1
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-24 22:43:14 -0500

    Card component just takes in card data now

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
-import {Box, Stack, Text} from '@chakra-ui/react'
+import {Box, Stack, Text, Flex} from '@chakra-ui/react'
 
-function Card({hidden, easy, hard}) {
-  return <Box p={10} boxShadow="lg" rounded={5} height="300" >
-    <Stack width="300">
-      <Text align="center" mb={5}>{hidden? "???": easy}</Text>
-      <Text>{hidden? "??????": hard}</Text>
-    </Stack>
+function Card({hidden, card}) {
+  return <Box p={10} boxShadow="lg" rounded={5} height="300" minWidth="200">
+    <Flex direction="column" align="center" width="100%" h="100%">
+      <Text fontSize="2xl" align="center" mb={10}>{hidden? "???": card.GOOD}</Text>
+      <Text fontSize="2xl" >{hidden? "??????": card.GREAT}</Text>
+    </Flex>
   </Box>;
 }
 
