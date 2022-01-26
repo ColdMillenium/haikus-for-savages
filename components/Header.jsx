58fea7394fa43b7723a382bba00bc34ebd6b7d8e
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-26 11:20:16 -0500

    changed header formatting

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,12 +4,19 @@ import { Flex , Spacer, Text} from '@chakra-ui/react';
 
 function Header() {
   const username = useStore(store => store.username)
-  return <Flex maxW="100%" align="center" justify="center" p={5} boxShadow="lg">
-    <Text fontSize="2xl" fontWeight="bolder">Haikus For Savages</Text>
-    <Spacer/>
-    <Text fontSize="lg">Welcome {username}</Text>
-    
-  </Flex>;
+  return <>
+    <Flex 
+      maxW="100%" 
+      align="center" 
+      justify="center" 
+      p={5} 
+      boxShadow="lg"
+    >
+      <Text fontSize="2xl" fontWeight="bolder">Haikus For Savages</Text>
+      <Spacer/>
+      <Text fontSize="lg">Welcome {username}</Text>
+    </Flex>
+  </>
 }
 
 export default Header;
