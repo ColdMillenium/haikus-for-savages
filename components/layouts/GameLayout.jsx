900ad8d4cf4d4827e51d1764f69725e874e1be50
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-26 11:20:57 -0500

    used portal to make header onto (not sure if helps)

--- components/layouts/GameLayout.jsx
+++ components/layouts/GameLayout.jsx
@@ -1,14 +1,18 @@
 
-import React from 'react';
+import React , {useRef} from 'react';
 import Header from '../Header'
-import {Center, Box} from '@chakra-ui/react'
+import {Center, Box, Portal} from '@chakra-ui/react'
 
 
 function GameLayout({children}) {
- 
+  const headerLocRef = useRef()
   return <Box h="100vh" w="100vw">
-    <Header/>
+    <Box ref={headerLocRef}></Box>
     {children}
+    <Portal containerRef={headerLocRef}>
+      <Header/>
+    </Portal>
+   
   </Box>;
 }
 
