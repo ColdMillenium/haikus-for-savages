1503219159c0cf9ed913772bcc92aba30641d3d4
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-26 10:56:35 -0500

    created a header component

---
+++ components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import useStore from '../store';
+import { Flex , Spacer, Text} from '@chakra-ui/react';
+
+function Header() {
+  const username = useStore(store => store.username)
+  return <Flex maxW="100%" align="center" justify="center" p={5} boxShadow="lg">
+    <Text fontSize="2xl" fontWeight="bolder">Haikus For Savages</Text>
+    <Spacer/>
+    <Text fontSize="lg">Welcome {username}</Text>
+    
+  </Flex>;
+}
+
+export default Header;
