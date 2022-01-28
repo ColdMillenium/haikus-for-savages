6a51598097e60ca74c3076f2d643543c1a88119f
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-28 00:36:33 -0500

    appearance: made card look like original game card

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-import {Box, Stack, Text, Flex} from '@chakra-ui/react'
+import {Box, Stack, Text, Flex, Center, Spacer} from '@chakra-ui/react'
+const primaryColor = "#f77238";
+const secondaryColor = "white"
 
 function Card({hidden, card}) {
   if(!card || !card.GOOD || !card.GREAT){
@@ -8,28 +10,72 @@ function Card({hidden, card}) {
     card.GOOD = "null Card?";
     card.GREAT = "check Logs";
   }
-  return <Box 
-    p={10} 
+  return <Center 
+    p={2} 
     boxShadow="lg" 
     rounded={5} 
-    height="300" 
-    minWidth="200" 
+    height={200} 
+    minWidth={150}
+    maxWidth={150} 
     m={5}
+    overflow="hidden"
   >
     <Flex 
       direction="column" 
       align="center" 
       width="100%" 
-      h="100%"
-    >
-      <Text fontSize="2xl" align="center" mb={10}>
-        {hidden? "???": card.GOOD}
-      </Text>
-      <Text fontSize="2xl">
-        {hidden? "??????": card.GREAT}
-      </Text>
+      h="100%" 
+      p={0}
+      m={0}
+    > 
+      <PhraseContainer>
+        <Points num={1} color={secondaryColor} backgroundColor={primaryColor}/>
+        <Text align="center" fontWeight="bold" fontSize="xl" color={primaryColor} mt={1}>
+          {hidden? "???": card.GOOD}
+        </Text>
+      </PhraseContainer>
+      
+      <PhraseContainer bottom={true}>
+        <Text align="center" fontWeight="bold" fontSize="lg" color={secondaryColor}>
+          {hidden? "??????": card.GREAT}
+        </Text>
+        <Spacer/>
+        <Points num={3} color={primaryColor} backgroundColor={secondaryColor}/>
+      </PhraseContainer>
     </Flex>
-  </Box>;
+  </Center>;
+}
+
+const Points = ({num, color, backgroundColor}) =>{
+  return <>
+    <Center 
+      backgroundColor={backgroundColor}
+      color={color}
+      w={4} 
+      h={4} 
+      rounded={100}
+      fontSize="sm"
+      fontWeight="bold"
+    >
+      {num}
+    </Center>
+  </>
+}
+const PhraseContainer = (props) =>{
+  const {bottom=false, children} = props;
+  return <Flex 
+    h={bottom?"55%":"45%"} 
+    align="center" 
+    direction="column" 
+    backgroundColor={bottom? primaryColor: secondaryColor}
+    p={2} 
+    m={0} 
+    width="100%"
+    borderBottomRadius	={bottom? 3 : 0}
+  >
+    {children}
+  </Flex>
+
 }
 
 export default Card;
