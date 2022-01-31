7217356f8a0e3c4c2037173dfb3268f658e02133
ColdMillenium <andrewgerribrowning@gmail.com>
2022-01-31 14:30:15 -0500

    Made one stop shop component for my icons w/ good names

---
+++ components/MyIcons.jsx
@@ -0,0 +1,8 @@
+import {IoEarOutline, IoSkull} from 'react-icons/io5'
+import {HiSpeakerphone } from 'react-icons/hi'
+import {AiOutlineStop } from 'react-icons/ai'
+
+export const SpectatorIcon = AiOutlineStop;
+export const SpeakerIcon = HiSpeakerphone;
+export const AudienceIcon = IoEarOutline;
+export const PunisherIcon = IoSkull;
