2247ced9ced2bf31683faf355c9a4e3a616d2488
ColdMillenium <andrewgerribrowning@gmail.com>
2022-02-02 15:16:15 -0500

    points earned animated, cards slide in and out, total points displayed

--- components/phase/Turn.jsx
+++ components/phase/Turn.jsx
@@ -5,6 +5,30 @@ import {Center, Stack, Flex, Button, Text, Box} from '@chakra-ui/react'
 import {Show, Hide} from '../Conditional';
 import PunishmentModal from '../PunishmentModal';
 import {SOUNDS} from '../../utils/effects'
+import styled, {keyframes} from 'styled-components';
+import { fadeOutLeft, fadeInRight, fadeIn, fadeOut} from 'react-animations';
+import Tada from 'react-reveal/Tada'
+const fadeOutLeftAnimation = keyframes`${fadeOutLeft}`;
+const fadeInRightAnimation = keyframes`${fadeInRight}`;
+
+
+const LastCardDiv = styled.div`
+  opacity: 0;
+  animation: 1s ${fadeOutLeftAnimation}
+`;
+
+const CurrCardDiv = styled.div`
+  animation: 1s ${fadeInRightAnimation}
+`;
+
+const fadeInAndOut = `
+  0%,100% { opacity: 0; }
+  50% { opacity: 1; }
+`;
+const PointsEarnedDiv = styled.div`
+  animation: 1s ${keyframes`${fadeInAndOut}`} 
+`;
+
 
 const soundVolume = 0.05;
 
@@ -21,30 +45,49 @@ function Turn() {
     punishmentInProgress,
     lastPointsEarned
   } = useStore(store => store.room)
-  const {clientId, clientsRole ,room } = useStore(store => store);
+  const {clientId, clientsRole ,room , lastCard,} = useStore(store => store);
   const playCard = useStore(store => (type) =>store.playerAction(store.ACTION.PLAY_CARD, type))
   const endTurn = useStore(store => () =>store.playerAction(store.ACTION.END_TURN))
   const executePunishment = useStore(store => () =>store.playerAction(store.ACTION.EXECUTE_PUNISHMENT))
   const [turnOver, setTurnOver] = useState(false);
+  const [startOfCard, setStartOfCard] = useState(true);
+  const totalPoints = playedCards.oops.length * -1 + 
+                      playedCards.good.length + 
+                      playedCards.great.length * 3;
+  const placeInPile = (type) =>{
+    setStartOfCard(false);
+    playCard(type);
+  }
   const hideCard = () =>{
     return clientsRole != "Punisher" && clientsRole!="Speaker"
   }
-  console.log(room);
-  if(lastPointsEarned == -1){
-    const sound = new Audio(SOUNDS.BRUH);
-    sound.volume = soundVolume
-    sound.play();
-  }else if (lastPointsEarned == 1){
-    const sound = new Audio(SOUNDS.WWE_RING_BELL);
-    sound.volume = soundVolume
-    sound.play();
-  }else if (lastPointsEarned == 3){
-    const sound = new Audio(SOUNDS.ANIME_WOW);
-    sound.volume = soundVolume
-    sound.play();
+  
+  if(startOfCard){
+    if(lastPointsEarned == -1){
+      const sound = new Audio(SOUNDS.BRUH);
+      sound.volume = soundVolume
+      sound.play();
+    }else if (lastPointsEarned == 1){
+      const sound = new Audio(SOUNDS.WWE_RING_BELL);
+      sound.volume = soundVolume
+      sound.play();
+    }else if (lastPointsEarned == 3){
+      const sound = new Audio(SOUNDS.ANIME_WOW);
+      sound.volume = soundVolume
+      sound.play();
+    }
   }
+  
+  //we use this to control the animations
+  useEffect(()=>{
+    setStartOfCard(true);
+    setTimeout(()=>{
+      setStartOfCard(false);
+    },1000)
+  }, [lastCard])
+
   return <Center h="100%" w="100%" overflow="hidden">
-    
+
     <Flex direction="column" align="center">
       <Text fontSize="4xl" fontWeight="bold">{clientsRole}</Text>
       <Hide when={punishmentInProgress}>
@@ -56,27 +99,76 @@ function Turn() {
         />
       </Hide>
       
-      <Center>
-        <Card hidden={hideCard()} card={currCard}/>
+      <Center width='fit-content'>
+        <Show when={!startOfCard}>
+          <Card hidden={hideCard()} card={currCard}/>
+        </Show>
+        <Show when={startOfCard}>
+          
+          {/* Current CARD */}
+          <CurrCardDiv>
+            <Box position="relative" top={0} left={lastCard? 90: 0} >
+              <Card hidden={hideCard()} card={currCard}/>
+            </Box>
+          </CurrCardDiv>
+          {/* Last card should be ontop of curr card and always shown */}
+          
+          <LastCardDiv>
+            <Show when={lastCard}>
+              <Box position="relative" top={0} right={100}>
+                <Card card={lastCard}/>
+              </Box>
+            </Show>
+          </LastCardDiv>
+      
+          <PointsEarnedDiv>
+            <Show when={lastPointsEarned!=0}>
+              <Tada>
+                <Text 
+                  position="relative" 
+                  top={-50} 
+                  right={100} 
+                  fontSize="3xl" 
+                  color={
+                    lastPointsEarned == 1? "yellow.500":
+                    lastPointsEarned == 3? "green.500":
+                    "red.500"
+                   
+                  }
+                  fontWeight="bold"
+                >
+                  {
+                    lastPointsEarned == 1? "+1":
+                    lastPointsEarned == 3? "+3":
+                    "-1"
+                  }
+                </Text>
+              </Tada>
+            </Show>
+          </PointsEarnedDiv>
+        </Show>
       </Center>
+      <Text fontSize="3xl" fontWeight="bold" color="gray.500">
+        {totalPoints} Pts
+      </Text>
       <Flex>
         <PileButton 
           disabled={turnOver || clientsRole != "Speaker"}
           type="OOPS" 
           score={playedCards.oops.length} 
-          onClick={playCard}
+          onClick={placeInPile}
         />
         <PileButton 
           disabled={turnOver || clientsRole != "Speaker"}
           type="GOOD" 
           score={playedCards.good.length} 
-          onClick={playCard}
+          onClick={placeInPile}
           />
         <PileButton 
           disabled={turnOver || clientsRole != "Speaker"}
           type="GREAT" 
           score={playedCards.great.length} 
-          onClick={playCard}
+          onClick={placeInPile}
         />
       </Flex>
       <Show when={!turnOver && clientsRole == "Audience"}>
@@ -145,6 +237,7 @@ const Timer = (props) => {
         if(Date.now() - timeStart > timeLeft){
           console.log("time is up!")
           setTurnOver(true)
+          clearInterval(interval);
           return;
         }
         if(time <= 0){
