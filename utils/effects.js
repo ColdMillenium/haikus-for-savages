2da3df938ee00e365c2d02d2cd2ac340d11263c3
ColdMillenium <andrewgerribrowning@gmail.com>
2022-02-01 19:03:05 -0500

    made a gifs eand effects util file

---
+++ utils/effects.js
@@ -0,0 +1,82 @@
+export const GIFS = {
+  DUM_DUM_HAT : 'https://i.giphy.com/media/26gswNS2Dm4q6600g/giphy.webp',
+  BAKA : "https://i.giphy.com/media/11jPPp3IdY1wEU/giphy.webp",
+  ZAWARUDO:'https://c.tenor.com/ie4QsP1jpukAAAAd/time-change-dance.gif',
+  TORTURE_DANCE :'https://i.giphy.com/media/TI9HiyUqRm75jPyKQ5/giphy.webp',
+  TAIGA_CRYING : "https://i.giphy.com/media/ROF8OQvDmxytW/giphy.webp",
+  ANIME_GIRL_CRYING :"https://i.giphy.com/media/8YutMatqkTfSE/giphy.webp",
+  STEVE_HARVY_ACTIVATED : "https://c.tenor.com/82xOKhz20KoAAAAC/steve-harvey-shocked.gif",
+  SPONGE_BOB_CAVEMEN:"https://c.tenor.com/3nA3bRItJkgAAAAC/spongebob-spongegar.gif",
+  BART_DUMPING_CAKE: "https://c.tenor.com/eTqdoJ96YP4AAAAd/failure-fail.gif",
+  SUBARU_FREAKOUT: "https://c.tenor.com/WtlvAQKd0s8AAAAC/anime-anime-cry.gif",
+  SUBARU_ZONEDOUT: "https://c.tenor.com/3Ov_pUxfvNMAAAAC/rezero-shinimodori.gif"
+
+}
+
+export const SOUNDS = {
+  RE_ZERO_CREEPY: "https://www.myinstants.com/media/sounds/ahhyooaaawhoaaa.mp3",
+  RE_ZERO_WEIRD: "https://www.myinstants.com/media/sounds/aa_lZXIehb.mp3",
+  YAMATE: "https://www.myinstants.com/media/sounds/combobreak_3.mp3",
+  YAMATE_KUDASAI: "https://www.myinstants.com/media/sounds/asus-yamete-kudasai-mp3cut_HkI6gb8.mp3",
+  BRUH: "https://www.myinstants.com/media/sounds/movie_1.mp3",
+  ZAWARUDO : "https://www.myinstants.com/media/sounds/hd-stardust-crusaders-za-warudo.mp3",
+  SHIT: "https://www.myinstants.com/media/sounds/kongoushit.mp3",
+  AMONG_US_ROUND: "https://www.myinstants.com/media/sounds/among-us-roundstart.mp3",
+  MM_WATCHA_SAY: "https://www.myinstants.com/media/sounds/mm-whatcha-say-meme-audio-mp3.mp3",
+  NOPE: "https://www.myinstants.com/media/sounds/engineer_no01_1.mp3",
+  ANIME_WOW: "https://www.myinstants.com/media/sounds/anime-wow-sound-effect.mp3",
+  NANI: "https://www.myinstants.com/media/sounds/nanimp3.mp3",
+  CAVEMAN_BONK: "https://www.myinstants.com/media/sounds/bonk_AgRFvsD.mp3",
+  LOSING_HORN: "https://www.myinstants.com/media/sounds/the-price-is-right-losing-horn.mp3,",
+  DUMMY_YEAH: "https://www.myinstants.com/media/sounds/dummy-yah.mp3",
+  ANTA_BAKA: "https://www.myinstants.com/media/sounds/anta-baka.mp3",
+  WWE_RING_BELL: "https://www.myinstants.com/media/sounds/wwe-bell.mp3" 
+}
+
+export const punishments = [
+  { 
+    gif : GIFS.DUM_DUM_HAT, 
+    sound: SOUNDS.DUMMY_YEAH
+  },
+  { 
+    gif : GIFS.ZAWARUDO, 
+    sound: SOUNDS.ZAWARUDO
+  },
+  { 
+    gif : GIFS.TORTURE_DANCE, 
+    sound: SOUNDS.NOPE,
+  },
+  { 
+    gif : GIFS.BAKA, 
+    sounds: SOUNDS.ANTA_BAKA
+  },
+  { 
+    gif : GIFS.ANIME_GIRL_CRYING, 
+    sounds: SOUNDS.YAMATE
+  },
+  { 
+    gif : GIFS.TAIGA_CRYING, 
+    sounds: SOUNDS.YAMATE_KUDASAI,
+  },
+  { 
+    gif : GIFS.STEVE_HARVY_ACTIVATED, 
+    sounds: SOUNDS.NANI,
+  },
+  { 
+    gif : GIFS.SPONGE_BOB_CAVEMEN, 
+    sounds: SOUNDS.CAVEMAN_BONK,
+  },
+  { 
+    gif : GIFS.BART_DUMPING_CAKE, 
+    sounds: SOUNDS.LOSING_HORN,
+  },
+  { 
+    gif : GIFS.SUBARU_FREAKOUT, 
+    sounds: SOUNDS.RE_ZERO_CREEPY,
+  },
+  { 
+    gif : GIFS.SUBARU_ZONEDOUT, 
+    sounds: SOUNDS.RE_ZERO_WEIRD,
+  },
+]
+
