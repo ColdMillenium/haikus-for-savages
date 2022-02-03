91322e1ade2ff72b1436b409ecaa537a383e8ddb
ColdMillenium <andrewgerribrowning@gmail.com>
2022-02-03 18:23:49 -0500

    added button in header to open speaker rules

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import useStore from '../store';
 import { Flex , Spacer, Text, Button} from '@chakra-ui/react';
+import SpeakerRulesModal from './SpeakerRulesModal'
 
 function Header() {
   const username = useStore(store => store.username)
   const toggleGameLog = useStore(store => store.toggleGameLog);
+  const [modalOpen, setModalOpen] = useState(false);
   return <>
     <Flex 
       maxW="100%" 
@@ -16,9 +18,11 @@ function Header() {
     >
       <Text fontSize="2xl" fontWeight="bolder">Haikus For Savages</Text>
       <Spacer/>
-      <Text fontSize="lg" fontWeight="bold" mr={5}>Welcome {username}</Text>
+      <Text fontSize="lg" fontWeight="bold" mr={4}>Welcome {username}</Text>
+      <Button mr={4} onClick={()=>setModalOpen(true)}>Rules</Button>
       <Button onClick={toggleGameLog}>Game Log</Button>
     </Flex>
+    <SpeakerRulesModal open={modalOpen} onClose={() => setModalOpen(false)}/>
   </>
 }
 
