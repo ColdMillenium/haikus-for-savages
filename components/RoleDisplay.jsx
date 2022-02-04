14a2f56f803a73ca78d9945d15d74e144a694010
ColdMillenium <andrewgerribrowning@gmail.com>
2022-02-04 17:03:47 -0500

    added description for roles and changed start button to ready button

--- components/RoleDisplay.jsx
+++ components/RoleDisplay.jsx
@@ -11,7 +11,6 @@ const RoleDisplay = props =>{
   const {backgroundColor} = props;
   const color = mode == 'TEAMS'? "white" : "black"
   const isSpectator =  mode == "TEAMS" && (clientsRole=="" || clientsRole=="Audience");
- 
   return <>
     <Flex 
       boxShadow="inner" 
@@ -24,8 +23,9 @@ const RoleDisplay = props =>{
     > 
       <Title data={{username, color, clientsRole}} />
       <RoleIcon data={{backgroundColor, clientsRole}}/>
+      <RoleDescription data={{clientsRole, color}}/>
+      <ReadyButton data={{roleReady, isSpectator, clientReady}}/>
       <WaitingDetails data={{readyToStart,clientReady, color, isSpectator}}/>
-      <StartButton data={{roleReady, isSpectator, clientReady}}/>
     </Flex>
   </>
 }
@@ -66,22 +66,32 @@ const RoleIcon = (props) =>{
     />
   </Center>
 }
+const RoleDescription = (props) =>{
+  const {clientsRole, color} = props.data
+  const description = {
+    "" : "It's the other team's turn. Turn off your mic off/be quiet and hope your team's punisher get's em good",
+    "Audience": "It's your team's turn. Listen carefully to your speaker and guess the word or phrase to earn points!", 
+    "Speaker" : "It's your team's turn. Speak carefully and give your teams clues to each word and/or phrase!",
+    "Punisher": "It's the other team's turn. Listen carefully to their Speaker and punish them if they break any of the rules!"
+  }
+  return <Text p={2} mb={3} color={color} fontSize="sm" align="center">{description[clientsRole]}</Text> 
+}
 
 const WaitingDetails = (props) =>{
   const {readyToStart,clientReady, color, isSpectator} = props.data;
   
-  return <Text color={color} fontSize="sm">
+  return <Text color={color} fontSize="sm" as="i">
     <Show when={readyToStart}>Waiting for Speaker to start the turn...</Show>
     <Show when={clientReady && !readyToStart}>Waiting for other players to ready...</Show>
     <Show when={!isSpectator && !clientReady && !readyToStart}>When you{`'`}re ready to begin, click ready</Show>
     <Show when={isSpectator && !readyToStart}>Waiting on other players to ready up...</Show>
   </Text>
 }
-const StartButton = (props) =>{
+const ReadyButton = (props) =>{
   const {roleReady, isSpectator, clientReady} = props.data;
   return  <Hide when={isSpectator}>
     <Button 
-      mt={3} 
+      m={3} 
       colorScheme={clientReady? 'red':'green'} 
       size="sm"
       onClick={roleReady}
